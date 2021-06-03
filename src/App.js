b4bf988076623e696d32da45b9962eec291d6afc
Charles Wang <wang.charles234@gmail.com>
2021-06-02 23:08:58 -0400

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Navbar";
+import Home from './Home';
+import Experience from './Experience';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <p>is this working lol!!!</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <h1>App</h1>
+        <div className="content">
+          <Switch>
+            <Route path="/experience">
+              <Experience />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+            
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Experience.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Experience() {
+  return (
+    <div>
+      <h1>Experience Page</h1>
+    </div>
+  );
+}
+
+export default Experience;
